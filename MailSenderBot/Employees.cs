d037b339cd7d7c46e4f4256b2c79a02fb257aa87
satur566 <satur566@gmail.com>
2020-08-12 17:52:39 +0300

    Edited class Employees.cs

--- MailSenderBot/Employees.cs
+++ MailSenderBot/Employees.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace MailSenderBot
+namespace MailSender
 {
-    class Employees
+    static class Employees
     {
+        private static readonly List<string> whosBirthdayIs = new List<string>();
+        private static string congratulationsString;
+
+        public static List<string> GetWhosBirthdayIs()
+        {
+            whosBirthdayIs.Sort();
+            return whosBirthdayIs;
+        }
+        public static void SetWhosBirthdayIs(string entry)
+        {
+            whosBirthdayIs.Add(entry);
+        }
+
+        public static string GetCongratulationsString()
+        {
+            return congratulationsString;
+        }
+        public static void SetCongratulationsString(List<string> list)
+        {
+            foreach (var item in list)
+            {
+                congratulationsString += item.Trim() + "<br>";
+            }
+        }
     }
 }
