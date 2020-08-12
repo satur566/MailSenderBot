70e0c2407a6a30e21712ba18d230a41699216b27
satur566 <satur566@gmail.com>
2020-08-12 18:01:02 +0300

    Added Employees.cs

---
+++ MailSenderBotFW/Employees.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace MailSender
+{
+    static class Employees
+    {
+        private static readonly List<string> whosBirthdayIs = new List<string>();
+        private static string congratulationsString;
+
+        public static List<string> GetWhosBirthdayIs()
+        {
+            whosBirthdayIs.Sort();
+            return whosBirthdayIs;
+        }
+        public static void SetWhosBirthdayIs(string entry)
+        {
+            whosBirthdayIs.Add(entry);
+        }
+
+        public static string GetCongratulationsString()
+        {
+            return congratulationsString;
+        }
+        public static void SetCongratulationsString(List<string> list)
+        {
+            foreach (var item in list)
+            {
+                congratulationsString += item.Trim() + "<br>";
+            }
+        }
+    }
+}
