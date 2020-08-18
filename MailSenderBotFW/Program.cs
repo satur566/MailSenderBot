37c2607741b1d6d3f4205f400c2c958f416c2677
Alexandr Maximov <satur566@gmail.com>
2020-08-18 17:31:14 +0300

    Make some tasks and stuff.

--- MailSenderBotFW/Program.cs
+++ MailSenderBotFW/Program.cs
@@ -28,76 +28,32 @@ namespace MailSender
                                 }
                                 break;
                             case "-help":
+                                //TODO: describe every parameter.
                                 break;
-                            case "-showconfigure":
+                            case "-showconfigure":                                
+                                Configs.GetSenderEmail(); //TODO: add some console output.
+                                Configs.GetSenderPassword();
+                                Configs.GetSenderName();
+                                Configs.GetEmailrecievers();
+                                Configs.GetMessageSubject();
+                                Configs.GetHtmlPath();
+                                Configs.GetXlsPath();
+                                Configs.GetEmployeeNameColumnNumber();
+                                Configs.GetBirthdayColumnNumber();
+                                Configs.GetServerAddress();
+                                Configs.GetServerPort();
+                                Configs.GetFiveDayMode();
+                                Configs.GetLogRecievers();
                                 break;
-
-
+                            case "-editconfig": //TODO: edit one or more parameters until '-' occured.
+                                break;
                             default:
                                 Console.WriteLine("Unknown parameter.");
                                 break;
                         }
                     }
                 }
-            }            
-            /*try
-            {
-                Methods.LoadConfig();
-            }
-            catch
-            {
-                Console.WriteLine("Cannot properly read config file. Please set configuration again.");
-                Console.Write("Set sender e-mail: ");
-                Methods.EditConfig("senderEmail", Console.ReadLine());
-
-                Console.Write("Set sender password: ");
-                Methods.EditConfig("senderPassword", Console.ReadLine()); //TODO: password enter with mask low priority.
-
-                Console.Write("Set sender displayed name: "); //TODO: return username
-                Methods.EditConfig("senderName", Console.ReadLine());
-
-                Console.Write("Set reciever e-mail: ");
-                Methods.EditConfig("emailRecievers", Console.ReadLine());
-
-                Console.Write("Set message subject: ");
-                Methods.EditConfig("messageSubject", Console.ReadLine());
-
-                Console.Write($"Set a path to html file: ");
-                Methods.EditConfig("htmlPath", Console.ReadLine());
-
-                Console.Write($"Set a path to xls file: ");
-                Methods.EditConfig("xlsPath", Console.ReadLine());
-
-                Console.Write("Set a number of column contains birthday dates: ");
-                Methods.EditConfig("birthdayColumnNumber", Console.ReadLine());
-
-                Console.Write("Set a number of column contains employees names: ");
-                Methods.EditConfig("employeeNameColumnNumber", Console.ReadLine());
-
-                Console.Write("Set server address: ");
-                Methods.EditConfig("serverAddress", Console.ReadLine());
-
-                Console.Write("Set server port (if default - leave empty): ");
-                Methods.EditConfig("serverPort", Console.ReadLine());
-
-                Console.WriteLine("Use 5/2 workmode?(yes) \nOtherwise will be user full week mode");
-                Methods.EditConfig("fiveDaysMode", Console.ReadLine());
-
-                Console.Write("Set logs recievers: ");
-                Methods.EditConfig("logRecievers", Console.ReadLine());
-                try
-                {
-                    File.WriteAllText(Configs.GetConfigPath(), string.Empty);
-                    File.WriteAllLines(Configs.GetConfigPath(), Configs.GetConfigurations().ToArray());
-                    Configs.AddLogsCollected($"Config save: SUCCESS.");
-                }
-                catch
-                {
-                    Configs.AddLogsCollected($"Config save: FAILURE.");
-                }
-                Methods.LoadConfig();
-            }
-            Methods.SendMail();*/
+            }                        
         }
     }
 }
